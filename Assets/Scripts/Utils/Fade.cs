d7a554780ffde0a6fbdfe3185d5ebfec792f9ec6
lilia.tsonya <lilia.tsonya@outlook.com>
2021-05-25 23:01:15 +0300

    added fade in/out script

---
+++ Assets/Scripts/Utils/Fade.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fade : MonoBehaviour
+{
+    [SerializeField] private Canvas _faderObj;
+    [SerializeField] private Image _faderImg;
+    [SerializeField] private Text _faderTxt;
+    [SerializeField] private float _fadeSpeed = .02f;
+    [SerializeField] private Color _fadeTransparency = new Color(0, 0, 0, .04f);
+
+    public IEnumerator FadeOut(Action callback)
+    {
+        _faderObj.enabled = true;
+
+        while (_faderImg.color.a < 1 && _faderTxt.color.a < 1)
+        {
+            _faderImg.color += _fadeTransparency;
+            _faderTxt.color += _fadeTransparency;
+            yield return new WaitForSeconds(_fadeSpeed);
+        }
+
+        callback?.Invoke();
+        //ActivateScene(); //Activate the scene when the fade ends
+    }
+
+    public IEnumerator FadeIn()
+    {
+        while (_faderImg.color.a > 0 && _faderTxt.color.a > 0)
+        {
+            _faderImg.color -= _fadeTransparency;
+            _faderTxt.color -= _fadeTransparency;
+            yield return new WaitForSeconds(_fadeSpeed);
+        }
+
+        _faderObj.enabled = false;
+    }
+}
+

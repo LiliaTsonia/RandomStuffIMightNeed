ef65e7aad5ad5f7b37270d839c75302032ee1846
lilia.tsonya <lilia.tsonya@outlook.com>
2021-07-21 10:05:57 +0300

    added google play services and FirebaseInit script in advanced

---
+++ Assets/Scripts/Firebase/FirebaseInit.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+//using Firebase;
+//using Firebase.Analytics;
+
+public class FirebaseInit : MonoBehaviour
+{
+    void Start()
+    {
+        //FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task =>
+        //{
+
+        //});
+    }
+}
